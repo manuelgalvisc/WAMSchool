f9a3c1e80a0b3f89590246fc49cd09ed86aa07c2
manuelgalvisc <mdgcardenas24@gmail.com>
2020-06-29 15:04:45 -0500

    Forntend del login v1.0

---
+++ Frontend/src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+
+import { SocialLoginModule, AuthServiceConfig } from "angularx-social-login";
+import { GoogleLoginProvider} from "angularx-social-login";
+
+let config = new AuthServiceConfig([
+  {
+    id: GoogleLoginProvider.PROVIDER_ID,
+    provider: new GoogleLoginProvider("961220959171-mqlnn5ukods9v4e82rr7uuhc8g2krhoj.apps.googleusercontent.com")
+  },
+]);
+
+export function provideConfig() {
+  return config;
+}
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    SocialLoginModule
+  ],
+  providers: [
+    {
+      provide: AuthServiceConfig,
+      useFactory: provideConfig
+    }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
