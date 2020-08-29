eb832230d2f537efc92afadd81b98d2a6041183d
manuelgalvisc <mdgcardenas24@gmail.com>
2020-08-29 16:58:53 -0500

    Ejemplo

---
+++ Frontend/src/app/visor/lista-oa/lista-oa.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-oa',
+  templateUrl: './lista-oa.component.html',
+  styleUrls: ['./lista-oa.component.css']
+})
+export class ListaOAComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
