5f2b1e505bd1bf20c05cb8e23952e57a7d7d66f4
manuelgalvisc <mdgcardenas24@gmail.com>
2020-09-04 15:02:28 -0500

    Actividad de arrastre

--- Frontend/src/app/modal-actividad-match/modal-actividad-match.component.ts
+++ Frontend/src/app/modal-actividad-match/modal-actividad-match.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { NgbModal, NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { ActividadEmparejamiento } from '../model/actividadEmparejamiento';
+import { DragulaService } from 'ng2-dragula';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-modal-actividad-match',
@@ -11,30 +13,50 @@ export class ModalActividadMatchComponent implements OnInit {
   @Input() actividad: ActividadEmparejamiento = new ActividadEmparejamiento();
   elementosIniciales: String[] = new Array();
   elementosAleatorios: String[] = new Array();
-  posicionesAleatorias: Number[] = new Array();
+  puntuacion: number;
 
   constructor(private modalService: NgbModal,
-              public activeModal: NgbActiveModal,) { }
+              public activeModal: NgbActiveModal,
+              private dragulaService: DragulaService,) {
+    this.dragulaService.createGroup("VAMPIRES", {
+
+    });
+
+    this.dragulaService.dropModel("VAMPIRES").subscribe(args => {
+      console.log(args);
+    });
+  }
 
   ngOnInit(): void {
     for (let index = 0; index < this.actividad.parejas.length; index++) {
       this.elementosIniciales.push(this.actividad.parejas[index].cadena1);
+      this.elementosAleatorios.push(this.actividad.parejas[index].cadena2);
     }
-    //this.numerosAleatorios();
+    this.elementosAleatorios = this.numerosAleatorios(this.elementosAleatorios);
   }
 
-  numerosAleatorios() {
-    let bandera: boolean = true;
-    while(bandera){
-      let numero = Math.floor(Math.random() * (this.actividad.parejas.length - 0)) + 0;
-      if(this.posicionesAleatorias.indexOf(numero) != -1) {
-        this.posicionesAleatorias.push(numero);
-      }
-      if(this.posicionesAleatorias.length == this.actividad.parejas.length) {
-        bandera = false;
+  numerosAleatorios(a: String[]): String[] {
+    var j, x, i;
+    for (i = a.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        x = a[i];
+        a[i] = a[j];
+        a[j] = x;
+    }
+    return a;
+  }
+
+  comprobar() {
+    this.puntuacion = 0;
+    for (let i = 0; i < this.actividad.parejas.length; i++) {
+      if(this.elementosAleatorios[i] === this.actividad.parejas[i].cadena2){
+        this.puntuacion = this.puntuacion + 1;
       }
     }
-    console.log(this.posicionesAleatorias);
-
+    if(this.puntuacion > this.actividad.parejas.length/2){
+        Swal.fire('Puntuación', `Has contestado ${this.puntuacion} de ${this.actividad.parejas.length}`, 'success');
+    } else {
+      Swal.fire('Puntuación', `Has contestado ${this.puntuacion} de ${this.actividad.parejas.length}`, 'error');
+    }
   }
 }
